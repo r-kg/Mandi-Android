81662bbd853da2f557a6415ef9911a98f060d0c2
RollinKG <gundaegi2@gmail.com>
2022-05-07 20:23:04 +0900

    - Add MainActivity

---
+++ app/src/main/java/com/rkg/mandi/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rkg.mandi.presentation.ui.main
+
+import android.os.Bundle
+import com.rkg.mandi.R
+import com.rkg.mandi.databinding.MainActivityBinding
+import com.rkg.mandi.presentation.ui.BaseActivity
+
+class MainActivity : BaseActivity<MainActivityBinding>(R.layout.main_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
