9e67fee03667aa60d0393355629da1abde759919
RollinKG <gundaegi2@gmail.com>
2022-05-07 20:29:21 +0900

    - Add MainViewModel via Hilt

--- app/src/main/java/com/rkg/mandi/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/rkg/mandi/presentation/ui/main/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.rkg.mandi.presentation.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.rkg.mandi.R
 import com.rkg.mandi.databinding.MainActivityBinding
 import com.rkg.mandi.presentation.ui.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<MainActivityBinding>(R.layout.main_activity) {
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.vm = viewModel
     }
 }
