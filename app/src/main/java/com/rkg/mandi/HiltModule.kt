4027ce08404d1a7069ed467b24d05754f4e2ca81
RollinKG <gundaegi2@gmail.com>
2022-05-10 23:02:31 +0900

    - Add Room Database via hilt

---
+++ app/src/main/java/com/rkg/mandi/HiltModule.kt
@@ -0,0 +1,22 @@
+package com.rkg.mandi
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.rkg.mandi.data.local.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HiltModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, AppDatabase::class.java, AppDatabase.DATABASE_LABEL).build()
+}
