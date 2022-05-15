83a34886ef7c447e55571dd6381d438df2c341c4
RollinKG <gundaegi2@gmail.com>
2022-05-15 22:22:37 +0900

    - Add Camera preview view
    - Add extra permission check for sdk > =29

--- app/src/main/java/com/rkg/mandi/presentation/ui/plant/PlantActivity.kt
+++ app/src/main/java/com/rkg/mandi/presentation/ui/plant/PlantActivity.kt
@@ -2,20 +2,28 @@ package com.rkg.mandi.presentation.ui.plant
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.rkg.mandi.R
 import com.rkg.mandi.databinding.PlantActivityBinding
 import com.rkg.mandi.presentation.ui.BaseActivity
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class PlantActivity : BaseActivity<PlantActivityBinding>(R.layout.plant_activity) {
+
+    private lateinit var cameraExecutor: ExecutorService
 
     private val cameraPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
             val denied = permissions.filter { (_, granted) -> !granted }
             if (denied.isEmpty()) {
-                // start Camera
+                startCamera()
             } else {
                 denied.firstNotNullOf { (permission, _) ->
                     when (permission) {
@@ -40,6 +48,7 @@ class PlantActivity : BaseActivity<PlantActivityBinding>(R.layout.plant_activity
         super.onCreate(savedInstanceState)
 
         requestCameraPermission()
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     private fun requestCameraPermission() {
@@ -50,11 +59,38 @@ class PlantActivity : BaseActivity<PlantActivityBinding>(R.layout.plant_activity
         if (cameraPermission == PackageManager.PERMISSION_GRANTED &&
             writeStoragePermission == PackageManager.PERMISSION_GRANTED
         ) {
-            //start Camera
+            startCamera()
         } else {
             cameraPermissionLauncher.launch(
-                arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                mutableListOf(Manifest.permission.CAMERA).apply {
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                        add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    }
+                }.toTypedArray()
             )
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.previewView.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
