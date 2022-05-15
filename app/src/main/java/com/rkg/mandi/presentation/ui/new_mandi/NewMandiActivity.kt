785ad955cdb8c5accabe1c3d1bf776e171c0fb1c
RollinKG <gundaegi2@gmail.com>
2022-05-15 16:13:53 +0900

    - Add NewMandiActivity

---
+++ app/src/main/java/com/rkg/mandi/presentation/ui/new_mandi/NewMandiActivity.kt
@@ -0,0 +1,16 @@
+package com.rkg.mandi.presentation.ui.new_mandi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class NewMandiActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
