3c765f8faa95ac2f1b23281026e112f39a5d0e77
RollinKG <gundaegi2@gmail.com>
2022-05-15 16:28:30 +0900

    - Add Appbar

--- app/src/main/java/com/rkg/mandi/presentation/ui/new_mandi/NewMandiActivity.kt
+++ app/src/main/java/com/rkg/mandi/presentation/ui/new_mandi/NewMandiActivity.kt
@@ -3,6 +3,15 @@ package com.rkg.mandi.presentation.ui.new_mandi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.rkg.mandi.R
 
 class NewMandiActivity : ComponentActivity() {
 
@@ -10,7 +19,34 @@ class NewMandiActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-
+            ActivityView()
         }
     }
+}
+
+@Composable
+fun ActivityView() {
+    Scaffold(
+        topBar = {
+            AppBar {
+
+            }
+        },
+    ) {
+
+    }
+}
+
+@Composable
+fun AppBar(onClick: () -> Unit) {
+    TopAppBar(
+        title = { },
+        backgroundColor = Color.White,
+        elevation = 3.dp,
+        navigationIcon = {
+            IconButton(onClick = onClick) {
+                Icon(painter = painterResource(id = R.drawable.ic_back), contentDescription = null)
+            }
+        }
+    )
 }
