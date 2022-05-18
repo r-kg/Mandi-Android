06779f6c214c515aa2ee146497ce583dbc5604b5
Kyle Kim <gundaegi2@gmail.com>
2022-05-18 16:34:27 +0900

    - Add updated, selectbyid query and functions

--- app/src/main/java/com/rkg/mandi/data/local/dao/MandiDao.kt
+++ app/src/main/java/com/rkg/mandi/data/local/dao/MandiDao.kt
@@ -1,9 +1,6 @@
 package com.rkg.mandi.data.local.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.rkg.mandi.data.model.MandiEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -11,7 +8,13 @@ import kotlinx.coroutines.flow.Flow
 interface MandiDao {
 
     @Query("SELECT * FROM MandiEntity")
-    fun selectAll() : Flow<List<MandiEntity>>
+    fun selectAll(): Flow<List<MandiEntity>>
+
+    @Query("SELECT * FROM MandiEntity WHERE id == :id")
+    suspend fun selectById(id: Int): List<MandiEntity>
+
+    @Update
+    suspend fun update(mandi: MandiEntity): Int
 
     @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun insert(mandi: MandiEntity)
